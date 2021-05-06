899fadb7e0474b18e759062ec769a5b93f494fd5
nundee <nundee@gmail.com>
2021-05-06 14:14:41 +0000

    Deploying to gh-pages from  @ 939c650c50ba5d21f695c9f8e276d1ab5864c940 🚀

---
+++ my_interop.js
@@ -0,0 +1,12 @@
+﻿window.MyJsLib = {
+    saveSettings: function(s) {
+        console.log("save settings", s)
+        localStorage.setItem("NotenTrainerSettings", s);
+    },
+
+    loadSettings: function() {
+        console.log("load settings")
+        const s = localStorage.getItem("NotenTrainerSettings")
+        return s == undefined || s == null ? "" : s
+    },
+};
